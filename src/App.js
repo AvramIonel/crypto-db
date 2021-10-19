53bb3faf4fbeea966896f4649b7ad9182bd79208
Ionel Florin Avram <avram.ionel.florin@gmail.com>
2021-10-19 21:06:28 +0300

    first steps in getting the data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import Button from 'react-bootstrap/Button'
-import './App.css';
+import React, { useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import { getCoinsMarket } from "./services/api";
 
 function App() {
+  useEffect(() => {
+    const getCoinsMarketParams = {
+      vs_currency: "eur",
+      per_page: 10,
+    };
+    getCoinsMarket(getCoinsMarketParams);
+  }, []);
+
   return (
     <div className="App">
       <Button variant="outline-success">Click Me!</Button>
---
+++ src/services/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://api.coingecko.com/api/v3",
+  headers: { Accept: "application/json" },
+});
+
+// {vs_currency: 'eur'}
+const buildParams = (params) => {
+  if (params === undefined) return "";
+  // console.log(params);
+  const arr = [];
+  Object.keys(params).forEach((k) => {
+    if (params[k]) arr.push(`${k}=${params[k]}`);
+  });
+  return "?" + arr.join("&");
+  // return "?vs_currency=eur&per_page=10";
+};
+
+export const getCoinsMarket = async (params) => {
+  let isValid = params !== undefined;
+  if (params) {
+    const paramsArray = Object.keys(params);
+    // console.log(paramsArray);
+    isValid = paramsArray.includes("vs_currency");
+  }
+  // console.log(isValid);
+
+  if (isValid) {
+    const parsedParams = buildParams(params);
+    // console.log(parsedParams)
+    return await instance.get(`/coins/markets${parsedParams}`);
+  }
+};
